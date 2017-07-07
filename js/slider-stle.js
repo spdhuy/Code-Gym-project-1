8617224aef946f1869a94100b5783340d1171389
Huy <huy@NAL40.local>
2017-07-07 14:24:55 +0700

    final

---
+++ js/slider-stle.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Huy on 7/7/2017.
+ */
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        minSlides: 2,
+        maxSlides: 5,
+        slideWidth: 170,
+        slideMargin: 10,
+        hideControlOnEnd: true
+    });
+})
+
